5e12ba99c0a6fa8faa140a4f05a0720bb1abc03a
Jamie Chang <jamie.cheng.chang@gmail.com>
2021-12-19 18:14:55 +0000

    d18: fix formatting

--- d18/part2.py
+++ d18/part2.py
@@ -4,4 +4,11 @@ from part1 import magnitude, parse_one, read
 
 
 if __name__ == "__main__":
-    print(max(magnitude(parse_one(a) + parse_one(b)) for a, b in permutations(read(), 2)))
+    print(
+        max(
+            magnitude(
+                parse_one(a) + parse_one(b),
+            )
+            for a, b in permutations(read(), 2)
+        )
+    )
